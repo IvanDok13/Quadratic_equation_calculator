bab4a26dbeebb7fb42db3e15f9f649f1bf34749f
Ivan Klochkov <113414074+IvanDok13@users.noreply.github.com>
2022-12-12 20:29:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,32 +13,40 @@ $(document).ready(function() {
          if(!a || !b || !c) {
               alert('Fill in all the fields!');
         } else {
+          
+         let d = b ** 2 - 4 * a * c;
+         console.log(d); 
+         
+         if (a === 0 && b !== 0) {
+                  let x = c / b;
 
-             let d = b ** 2 - 4 * a * c;
-             console.log(d);
+                  $('.1').text(x);
+           } else if (a !== 0 && b === 0) {
+                  let x = (c / a) ** 0.5;
 
-             if(d < 0) {
-                $('.1').text('No solutions');
-                $('.2').text('No solutions');
-             } else if(d == 0) {
+             $('.1').text(x);
+           } else if(d < 0) {
+               
+                  $('.1').text('No solutions');
+                  $('.2').text('No solutions');
+           } else if(d === 0) {
 
-                let x = -b / 2 * a;
-                console.log(x);
+                  let x = -b / 2 * a;
+                  console.log(x);
 
-                $('.1').text(x);
-                $('.2').text('');
-             } else if(d > 0) {
+                  $('.1').text(x);
+                  $('.2').text('');
+           } else if(d > 0) {
 
-                let x1 = (-b + Math.sqrt(d)) / (2 * a);
-                let x2 = (-b - Math.sqrt(d)) / (2 * a);
+                  let x1 = (-b + Math.sqrt(d)) / (2 * a);
+                  let x2 = (-b - Math.sqrt(d)) / (2 * a);
+                  console.log(x1, x2);
 
-                console.log(x1, x2);
+                  $('.1').text(x1);
+                  $('.2').text(x2);
+               }
 
-                $('.1').text(x1);
-                $('.2').text(x2);
-             }
-        }
+            }
+        })
 
     })
-
-})
