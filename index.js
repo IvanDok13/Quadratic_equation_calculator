5e82f377f9839910b975549bcce2c3ce8317afb1
Ivan Klochkov <113414074+IvanDok13@users.noreply.github.com>
2022-12-16 18:14:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,45 @@
 $(document).ready(function() {
     $('#enter').on('click', function() {
 
-        $('.text').text('');
-        $('.x1').text('');
-        $('.x2').text('');
+       $('.text').text('');
+       $('.x1').text('');
+       $('.x2').text('');
+       
+        let a = $('#coof-a').val();
+        let b = $('#coof-b').val();
+        let c = $('#coof-c').val();
 
-
-         let a = $('#coof-a').val();
-         let b = $('#coof-b').val();
-         let c = $('#coof-c').val();
-
-         if(!a || !b || !c) {
+        if(!a || !b || !c) {
               alert('Fill in all the fields!');
         } else {
-          
-         let d = b ** 2 - 4 * a * c;
-         console.log(d); 
-         
-         if (a === 0 && b !== 0) {
+               
+           if (a === 0 && b !== 0) {
                   let x = c / b;
+                  console.log(x);
 
                   $('.1').text(x);
            } else if (a !== 0 && b === 0) {
                   let x = (c / a) ** 0.5;
-
-             $('.1').text(x);
-           } else if(d < 0) {
+                  console.log(d);
+               
+                  $('.1').text(x);
+            } else {
+                
+                let d = b ** 2 - 4 * a * c;
+                console.log(d); 
+                
+                if (a !== 0 && b !== 0 && d < 0) {
                
                   $('.1').text('No solutions');
                   $('.2').text('No solutions');
-           } else if(d === 0) {
+                } else if (a !== 0 && b !== 0 && d === 0) {
 
                   let x = -b / 2 * a;
                   console.log(x);
 
                   $('.1').text(x);
                   $('.2').text('');
-           } else if(d > 0) {
+                } else if (a !== 0 && b !== 0 && d > 0) {
 
                   let x1 = (-b + Math.sqrt(d)) / (2 * a);
                   let x2 = (-b - Math.sqrt(d)) / (2 * a);
@@ -44,7 +47,7 @@ $(document).ready(function() {
 
                   $('.1').text(x1);
                   $('.2').text(x2);
-               }
+             }
 
             }
         })
